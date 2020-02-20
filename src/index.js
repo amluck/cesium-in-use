6254fec3e878d195918a17d14e3876471aee6d17
Zenway Chen <weinanforeve@gmail.com>
2020-02-20 14:20:19 +0800

    1.添加navigition组件 2.修改webpack.config,json报错的问题

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { Viewer } from 'cesium'
+import { Viewer, Cartographic } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 import './css/main.css'
+import viewerCesiumNavigationMixin from '@znemz/cesium-navigation'
+import '@znemz/cesium-navigation/dist/index.css'
 
 var viewer = new Viewer('cesiumContainer', {
   baseLayerPicker: false,
@@ -18,3 +20,16 @@ viewer._cesiumWidget._creditContainer.style.display = 'none'
 viewer.scene.globe.showGroundAtmosphere = false
 //FPS 帧率显示
 // viewer.scene.debugShowFramesPerSecond = true;
+
+var options = {}
+// 用于在使用重置导航重置地图视图时设置默认视图控制。接受的值是Cesium.Cartographic 和 Cesium.Rectangle.
+options.defaultResetView = Cartographic.fromDegrees(116.39, 39.9, 5000000.0)
+// 用于启用或禁用罗盘。true是启用罗盘，false是禁用罗盘。默认值为true。如果将选项设置为false，则罗盘将不会添加到地图中。
+options.enableCompass = true
+// 用于启用或禁用缩放控件。true是启用，false是禁用。默认值为true。如果将选项设置为false，则缩放控件将不会添加到地图中。
+options.enableZoomControls = true
+// 用于启用或禁用距离图例。true是启用，false是禁用。默认值为true。如果将选项设置为false，距离图例将不会添加到地图中。
+options.enableDistanceLegend = true
+// 用于启用或禁用指南针外环。true是启用，false是禁用。默认值为true。如果将选项设置为false，则该环将可见但无效。
+options.enableCompassOuterRing = true
+viewerCesiumNavigationMixin(viewer, options)
