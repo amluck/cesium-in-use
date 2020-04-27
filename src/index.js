3317f296e78ac8d8f4a6a298c9ffaa893c4dbd78
Zenway Chen <weinanforeve@gmail.com>
2020-04-27 11:38:54 +0800

    1. 更新依赖到最新版本 2.  底图换用谷歌，不使用默认的Ion就不会报token的错误

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Viewer, Cartographic } from 'cesium'
+import { Viewer, Cartographic, UrlTemplateImageryProvider } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 import './css/main.css'
 import viewerCesiumNavigationMixin from '@znemz/cesium-navigation'
@@ -13,7 +13,12 @@ var viewer = new Viewer('cesiumContainer', {
   sceneModePicker: false,
   timeline: false,
   animation: false,
-  scene3DOnly: true
+  scene3DOnly: true,
+  imageryProvider: googleMapProvider,
+})
+
+var googleMapProvider = new UrlTemplateImageryProvider({
+  url: 'http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&x={x}&y={y}&z={z}&s=Gali',
 })
 viewer._cesiumWidget._creditContainer.style.display = 'none'
 //地面大气效果
@@ -22,14 +27,9 @@ viewer.scene.globe.showGroundAtmosphere = false
 // viewer.scene.debugShowFramesPerSecond = true;
 
 var options = {}
-// 用于在使用重置导航重置地图视图时设置默认视图控制。接受的值是Cesium.Cartographic 和 Cesium.Rectangle.
 options.defaultResetView = Cartographic.fromDegrees(116.39, 39.9, 5000000.0)
-// 用于启用或禁用罗盘。true是启用罗盘，false是禁用罗盘。默认值为true。如果将选项设置为false，则罗盘将不会添加到地图中。
 options.enableCompass = true
-// 用于启用或禁用缩放控件。true是启用，false是禁用。默认值为true。如果将选项设置为false，则缩放控件将不会添加到地图中。
 options.enableZoomControls = true
-// 用于启用或禁用距离图例。true是启用，false是禁用。默认值为true。如果将选项设置为false，距离图例将不会添加到地图中。
 options.enableDistanceLegend = true
-// 用于启用或禁用指南针外环。true是启用，false是禁用。默认值为true。如果将选项设置为false，则该环将可见但无效。
 options.enableCompassOuterRing = true
 viewerCesiumNavigationMixin(viewer, options)
